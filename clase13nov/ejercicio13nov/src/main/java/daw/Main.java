a5145fae20824838752cf98aceb8dbec5d6940af
usuario <aruiagu307@g.educaand.es>
2025-11-14 10:18:27 +0100

    memuero

--- clase13nov/ejercicio13nov/src/main/java/daw/Main.java
+++ clase13nov/ejercicio13nov/src/main/java/daw/Main.java
@@ -6,36 +6,82 @@ public class Main {
     public static void main(String[] args) {
         // Crea una función que devuelva un array de tamaño especificado en el parámetro
         // con número aleatorios entre 1-30
-        // Función que recibe un array de números (array del paso 7) y devuelve
+
+        // Función que recibe un array de números  y devuelve
         // un nuevo array sin duplicados, IGNORA ESTO, solo los de arriba
         Random random = new Random();
 
-        int juanito = random.nextInt(0, 31);
+        int tamanio = random.nextInt(0, 31);
 
-        for (int i = 0; i < juanito; i++) {
+        int[] ArrayMain;
 
-            System.out.println(mamahuevo(juanito));
+        ArrayMain = funcion(tamanio);
+
+        for (int i = 0; i < ArrayMain.length; i++) {
+
+            System.out.println(ArrayMain[i]);
 
         }
 
-    }
+        // Func
 
-    public static int[] mamahuevo(int juanito) {
+        int [] arraySinDuplicados = recibirArray(ArrayMain);
+
+    }
+         // VOZ DE DIEGO EN MI CABEZA: ADRIAN ESTO ES UN PUTO PARAMETRO (,......)
+    public static int[] funcion(int tamanio) {
 
         int[] ArrayInt;
 
-        ArrayInt = new int[juanito];
+        ArrayInt = new int[tamanio];
 
         Random random = new Random();
 
         for (int i = 0; i < ArrayInt.length; i++) {
 
-            ArrayInt[i] = random.nextInt(0, 31);
+            ArrayInt[i] = random.nextInt(1, 31);
 
             System.out.println("El tamaño del array es " + ArrayInt.length + " y su posición es " + ArrayInt[i]);
 
         }
 
         return ArrayInt;
+    }
+
+    public static int[] recibirArray(int[] array) {
+
+        // Creas un array del tamaño de array
+
+        // Recorres array
+
+        // Si el numero de la posición x no está en el array nuevo lo incluyes en la misma posición x
+        if (buscarNumero(arry[]i, array))
+
+        int[] array1 = new int[];
+        //Array de funcion j
+
+
+        for (int i = 0; i < ArrayInt.length; i++, j++) { 
+            if (ArrayInt[i] == ArrayInt[j]) {    Arraynuevo[i] =  ArrayInt[i];
+
+
+
+
+            }
+
+        }
+        int loquesea = 0;
+
+        return loquesea;
+    }
+
+    // Función buscar un número en un array
+    public static boolean buscarNumero(int numero, int[] array){
+        for ( int i = 0; i < array.length; i++) {
+            if (numero == array[i]){
+                return true;
+            }
+        }
+        return false;
     }
 }
